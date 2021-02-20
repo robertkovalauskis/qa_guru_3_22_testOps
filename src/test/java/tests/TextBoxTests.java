6cb9044d1f5f9da4066613b8c916951fa72e7569
Robert Kovalauskis <robert.kovalauskis@gmail.com>
2021-02-20 12:30:16 +0200

    Changed harcoded values to dynamic

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,9 +1,14 @@
 package tests;
 
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.cssClass;
 import static com.codeborne.selenide.Condition.text;
@@ -14,9 +19,16 @@ import static io.qameta.allure.Allure.step;
 @Story("Text box")
 @Feature("Fill the Text box")
 public class TextBoxTests extends TestBase {
+    Faker faker = new Faker();
+    FakeValuesService fakeValuesService = new FakeValuesService(
+            new Locale("en-GB"), new RandomService());
+    String firstName = faker.name().firstName();
+    String lastName = faker.name().lastName();
+    String userEmail = fakeValuesService.bothify("????##@gmail.com");
+    String userNumber = fakeValuesService.regexify("[0-9]{10}");
+    String currentAddress = faker.address().fullAddress();
 
     @Test
-
     @DisplayName("Successful filling of the text box")
     void fillFormTest() {
         step("Open Text Box form", () -> {
@@ -25,17 +37,17 @@ public class TextBoxTests extends TestBase {
         });
 
         step("Fill Text Box form", () -> {
-            $("#userName").val("Alex");
-            $("#userEmail").val("Egorov@alex.com");
-            $("#currentAddress").val("Montenegro");
+            $("#userName").val(firstName);
+            $("#userEmail").val(userEmail);
+            $("#currentAddress").val(currentAddress);
             $("#permanentAddress").val("Street 1");
             $("#submit").click();
         });
 
         step("Verify successful form submit", () -> {
-            $("#output").shouldHave(text("Name:Alex\n" +
-                    "Email:Egorov@alex.com\n" +
-                    "Current Address :Montenegro\n" +
+            $("#output").shouldHave(text("Name:" + firstName +
+                    "Email:" + userEmail +
+                    "Current Address : " + currentAddress +
                     "Permananet Address :Street 1"));
         });
     }
